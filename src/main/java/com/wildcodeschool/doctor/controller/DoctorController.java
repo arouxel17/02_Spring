dbef666875db74e9008fbb5ba4810c77c4a596b7
arouxel17 <alexandrerouxel17@gmail.com>
2023-03-07 11:09:30 +0100

    correction 1-12

--- src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
+++ src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -16,7 +16,7 @@ public class DoctorController {
     public Doctor doctor(@PathVariable int number) {
         if (number == 13) {
             return new Doctor(13, "Jodie Whittaker");
-        } else if (number < 13) {
+        } else if (number >= 1 && number <= 12) {
             throw new ResponseStatusException(HttpStatus.SEE_OTHER, "303");
         }
         throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Impossible de récupérer l'incarnation " + number);
